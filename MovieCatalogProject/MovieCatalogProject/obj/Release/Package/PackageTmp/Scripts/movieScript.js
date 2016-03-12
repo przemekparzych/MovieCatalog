1e1c943346d9cf01adfa336012902c2a30d88f86
przemekparzych <pp.18@op.pl>
2016-03-12 10:21:39 +0100

    update

---
+++ MovieCatalogProject/MovieCatalogProject/obj/Release/Package/PackageTmp/Scripts/movieScript.js
@@ -0,0 +1,16 @@
+﻿/// <reference path="angular.js" />
+
+
+var movies;
+var movieApp = angular
+    .module("movieModule", [])
+    .controller("movieController", ['$scope',function ($scope) {
+        $scope.casts = [];
+        $scope.casts.push($scope.addMe);
+        var array = [];
+        $scope.addItem = function (val) {
+           
+            array.push(val);
+            $scope.casts=array;
+        }
+    }]);
---
+++ MovieCatalogProject/MovieCatalogProject/obj/Release/Package/PackageTmp/Scripts/navScript.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.js" />
+
+
+var movieApp = angular
+    .module("navModule", [])
+    .controller("navController", ['$scope', function ($scope) {
+        $scope.index = 0;
+        $scope.setIndex = function (val) {
+            $scope.index = val;
+        }
+        $scope.getIndex = function () {
+            return($scope.index);
+        }
+    }]);
